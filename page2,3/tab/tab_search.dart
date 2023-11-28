82a3f7ce30e7eaeea20a2219e01569a5a3e63aef
hoyeoy <105593202+hoyeoy@users.noreply.github.com>
2023-11-28 15:45:42 +0900

    Add files via upload

---
+++ page2,3/tab/tab_search.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class TabSearch extends StatelessWidget {
+  const TabSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Tab Search'),
+    );
+  }
+}
